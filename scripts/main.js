c46753dd05d8050bb2d3dfeea3502f1bb82ec91b
d.georgiev.91 <d.georgiev.91@gmail.com>
2014-06-12 12:19:59 +0300

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,58 @@
+﻿/// <reference path="kinetic-v5.1.0.js" />
+'use strict';
+
+var stage = new Kinetic.Stage({
+    container: 'canvas-container',
+    width: 660,
+    height: 600
+});
+
+var layer = new Kinetic.Layer();
+
+var imageObj = new Image();
+imageObj.onload = function () {
+    var blob = new Kinetic.Sprite({
+        x: 350,
+        y: 40,
+        image: imageObj,
+        animation: 'idle',
+        animations: {
+            idle: [
+              0, 0, 31, 42,
+            ],
+            move: [
+              37, 0, 39, 42,
+              86, 0, 29, 42,
+              126, 3, 34, 39,
+            ]
+        },
+        frameRate: 7,
+        frameIndex: 0
+    });
+
+    layer.add(blob);
+    stage.add(layer);
+    blob.start();
+
+    document.addEventListener('keyup', function () {
+        blob.animation('idle');
+    });
+
+    document.addEventListener('keydown', function (e) {
+        if (e.keyCode == 39) {
+            blob.scaleX(1);
+        } else if (e.keyCode == 37) {
+            blob.scaleX(-1);
+            blob.x(blob)
+        } else {
+            return;
+        }
+
+        blob.animation('move');
+        var currentX = blob.x();
+        currentX += blob.scaleX() * 10
+        blob.x(currentX)
+    }, false);
+};
+
+imageObj.src = "images/mario-sprites.png";
